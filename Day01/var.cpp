210df544ee094451b87588daed089d83c9c23e9c
JeonJeo <158244859+JeonJeo@users.noreply.github.com>
2024-01-31 12:48:41 +0900

    Add files via upload

---
+++ Day01/var.cpp
@@ -0,0 +1,18 @@
+//var.cpp
+#include<stdio.h>
+
+int main() {
+
+	// 30 + 20: 사람은 연산과 기억을 동시에 함
+	// 30 + 20: 컴퓨터는 연산과 기억을 따로 순차적으로 함
+	
+	// 정수/실수/... 변수
+	// int - integer[독일어 정수]
+	int num = 100;
+	int son = 7;
+
+
+	printf("num의 값은: %d\n", num);
+	printf("num + son = %d\n", num + son);
+	return 0;
+}
